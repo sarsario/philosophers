f009bbe1cde066a87e3483886967d1bd36b15e53
osarsari <o.sarsari@gmail.com>
2023-10-21 14:16:03 +0200

    starting bonus

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 14:12:47 by osarsari          #+#    #+#             */
+/*   Updated: 2023/10/21 14:15:46 by osarsari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+#endif
